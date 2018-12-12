7e350d33027ba85b7612431c6c4210f1b64aa569
wenhai0596 <wenhai_xm@qq.com>
2018-12-12 13:47:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var request = require('request');
 var Service, Characteristic;
+
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory('homebridge-espeasy-switch-lai', 'ESPEasyLai', ESPEasyLai);
-}
-function ESPEasyLai(log, config) {
+    homebridge.registerAccessory('homebridge-espeasy-switch-lai', 'ESPEasyLai', ESPEasy_Lai);
+};
+
+function ESPEasy_Lai(log, config) {
     this.log = log;
     this.name = config.name || 'ESPEasySwitch';
     this.type = config.type || 'switch';
@@ -17,7 +19,7 @@ function ESPEasyLai(log, config) {
     this.duration = config.duration || 60;
     
     if (!this.ip) {
-        throw new Error('Your must provide IP address of the switch.');
+        throw new Error('Your must provide IP address of the switch');
 	}
 
     switch (this.type) {
@@ -34,17 +36,17 @@ function ESPEasyLai(log, config) {
 
     this.serviceInfo
         .setCharacteristic(Characteristic.Manufacturer, 'Lai')
-        .setCharacteristic(Characteristic.Model, 'espessy')
+        .setCharacteristic(Characteristic.Model, 'espeasy')
         .setCharacteristic(Characteristic.SerialNumber, 'abcd-1234-5678');
 
     this.service
         .getCharacteristic(Characteristic.On)
-        .on('get', this.getPowerState.bind(this))
-        .on('set', this.setPowerState.bind(this));
+        .on('get', this.getPowerS.bind(this))
+        .on('set', this.setPowerS.bind(this));
 }
 
-ESPEasyLai.prototype = {
-    getPowerState: function(callback) {
+ESPEasy_Lai.prototype = {
+    getPowerS: function(callback) {
         var log = this.log;
 		if (this.pulse) {
 			callback(null, false);
@@ -53,7 +55,7 @@ ESPEasyLai.prototype = {
 
         request.get({
             url: 'http://' + this.ip + '/control?cmd=status,' + this.doorRelayPin,
-            timeout: 1200
+            timeout: 12000
         }, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 var json = JSON.parse(body);
@@ -62,13 +64,13 @@ ESPEasyLai.prototype = {
                 return;
             }
 
-            log.debug('Error getting power state. (%s)', error);
+            log.debug('Error getting power state: (%s)', error);
 
             callback();
         });
     },
 
-    setPowerState: function(state, callback) {
+    setPowerS: function(state, callback) {
         var log = this.log;
 		var command = '/control?cmd=GPIO,' + this.doorRelayPin + ((state) ? 1: 0);
 
